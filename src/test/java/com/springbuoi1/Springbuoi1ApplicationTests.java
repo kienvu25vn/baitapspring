ab79e93c459480b988d2319d1dc2d6e5046d2a4f
Vu Trung Kien <bovang113@gmail.com>
2022-06-30 10:20:23 +0700

    commit

---
+++ src/test/java/com/springbuoi1/Springbuoi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbuoi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbuoi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
